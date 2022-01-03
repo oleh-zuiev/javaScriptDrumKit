43ad75c1508178587b169de7f85537593ef83a2e
oleh-zuiev <64410055+oleh-zuiev@users.noreply.github.com>
2022-01-04 00:14:03 +0200

    plays sounds without animation
    plays sounds without animation

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 'use strict'
 window.addEventListener('keydown', function (e) {
-    // console.log(event);
-    console.log(e);
-})
+    // console.log(e);
+    // console.log(e.key);
+    // console.log(typeof(e.code));
+    // console.log(e.code);
+    const audio = document.querySelector(`audio[data-key='${e.code}']`);
+    if (!audio) return;
+    // console.log(audio);
+    audio.currentTime = 0;
+    audio.play();
+});
+window.addEventListener('click', function (e) {
+    console.dir(e.target);
+    // console.log(e.key);
+    // console.log(typeof(e.code));
+    // console.log(e.code);
+    // const audio = document.querySelector(`audio[data-key='${e.code}']`);
+    // if (!audio) return;
+    // // console.log(audio);
+    // audio.currentTime = 0;
+    // audio.play();
+});
